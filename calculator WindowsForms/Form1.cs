91ef75b4796343b52b6c1ecf5787d321cdf58423
rushingfox <jianhaowu2000@gmail.com>
2020-09-14 09:14:00 +0800

    first commit

---
+++ calculator WindowsForms/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator_WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double number1 = Convert.ToDouble(textBox1.Text);
+            double number2 = Convert.ToDouble(textBox2.Text);
+            string result="error!";
+            if (radioButton1.Checked)
+            {
+                result = Convert.ToString(number1 + number2);
+            }
+            else if (radioButton2.Checked)
+            {
+                result = Convert.ToString(number1 - number2);
+            }
+            else if (radioButton3.Checked)
+            {
+                result = Convert.ToString(number1 * number2);
+            }
+            else if (radioButton4.Checked)
+            {
+                if (number2==0)
+                {
+                    MessageBox.Show("0 can not be the number 2 when the operator is /");
+                }
+                else
+                {
+                    result = Convert.ToString(number1 / number2);
+                }
+            }
+            else
+            {
+                MessageBox.Show("you have not chosen the operator!");
+            }
+
+            label4.Text = result;
+        }
+    }
+}
---
+++ calculator console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( @"enter two numbers and an operator in order(when you finish every entering you should enter '\n' )" );
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+L:          char c = Convert.ToChar(Console.ReadLine());
+            double result;
+            switch (c)
+            {
+                case '*': result = a * b;break;
+                case '+':result = a + b;break;
+                case '-':result = a - b;break;
+                case '/':result = a / b;break;
+                default:
+                    Console.WriteLine("you can only enter + - * /, please enter again!");
+                    goto L;
+            }
+            Console.WriteLine("the answer is " + result);
+            Console.WriteLine("enter any key to quit!");
+            Console.ReadKey();
+        }
+    }
+}
