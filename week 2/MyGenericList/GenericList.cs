06a91c87bd66bdefb50d49ea1e32a1b286168567
rushingfox <jianhaowu2000@gmail.com>
2020-09-25 21:29:49 +0800

    newest commit

---
+++ week 2/MyGenericList/GenericList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGenericList
+{
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> nodepointer = this.head;
+            while (nodepointer!=null)
+            {
+                action(nodepointer.Data);
+                nodepointer = nodepointer.Next;
+            }
+        }
+
+    }
+}
---
+++ week 2/MyGenericList/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> test = new GenericList<int>();//init the list
+            Console.WriteLine("please enter the number of nodes you want:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                test.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+
+            test.ForEach(s => Console.WriteLine(s));//通过调用这个方法打印链表元素
+            if (test.Head!=null)                    //通过调用这个方法求最大值和最小值
+            {
+                int max = test.Head.Data;
+                int min = test.Head.Data;
+                test.ForEach(s =>
+                {
+                    if (s > max)
+                    {
+                        max = s;
+                    }
+                    else if (s < min)
+                    {
+                        min = s;
+                    }
+                });
+                Console.WriteLine("the max value: " + max + " the min value: " + min);
+            }
+            int sum = 0;
+            test.ForEach(s =>  sum += s );
+            Console.WriteLine("the sum is " + sum); //通过调用这个方法求和
+
+            Console.ReadKey();
+        }
+    }
+}
