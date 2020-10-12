1d22db5a29b290482416989fb3a0c7b27d697757
rushingfox <jianhaowu2000@gmail.com>
2020-10-12 16:00:56 +0800

    newest commit

---
+++ week 4/CayleyTree/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace CayleyTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar3 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar4 = new System.Windows.Forms.HScrollBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.label10 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(795, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(12, 363);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(100, 21);
+            this.hScrollBar1.TabIndex = 1;
+            this.hScrollBar1.Tag = "";
+            this.hScrollBar1.ValueChanged += new System.EventHandler(this.hScrollBar1_ValueChanged);
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Location = new System.Drawing.Point(12, 399);
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(100, 21);
+            this.hScrollBar2.TabIndex = 2;
+            this.hScrollBar2.ValueChanged += new System.EventHandler(this.hScrollBar2_ValueChanged);
+            // 
+            // hScrollBar3
+            // 
+            this.hScrollBar3.Location = new System.Drawing.Point(12, 439);
+            this.hScrollBar3.Name = "hScrollBar3";
+            this.hScrollBar3.Size = new System.Drawing.Size(100, 21);
+            this.hScrollBar3.TabIndex = 3;
+            this.hScrollBar3.ValueChanged += new System.EventHandler(this.hScrollBar3_ValueChanged);
+            // 
+            // hScrollBar4
+            // 
+            this.hScrollBar4.Location = new System.Drawing.Point(12, 479);
+            this.hScrollBar4.Name = "hScrollBar4";
+            this.hScrollBar4.Size = new System.Drawing.Size(100, 21);
+            this.hScrollBar4.TabIndex = 4;
+            this.hScrollBar4.ValueChanged += new System.EventHandler(this.hScrollBar4_ValueChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(766, 402);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 8;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 384);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "右子树角度";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 424);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "左子树角度";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 464);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "右子树长度比例";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 505);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "左子树长度比例";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(116, 383);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(116, 424);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(131, 464);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 15);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "0.6";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(130, 505);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 15);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "0.7";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(764, 461);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(114, 15);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "位置系数：0.80";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(795, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 59);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "choose color";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(795, 152);
+            this.vScrollBar1.Maximum = 29;
+            this.vScrollBar1.Minimum = 5;
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(21, 190);
+            this.vScrollBar1.TabIndex = 17;
+            this.vScrollBar1.Value = 5;
+            this.vScrollBar1.ValueChanged += new System.EventHandler(this.vScrollBar1_ValueChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(792, 352);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(90, 15);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "递归深度：5";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(767, 489);
+            this.trackBar2.Maximum = 150;
+            this.trackBar2.Minimum = 10;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 56);
+            this.trackBar2.TabIndex = 19;
+            this.trackBar2.Value = 50;
+            this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(764, 548);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(98, 15);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "主干长度：50";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 638);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.hScrollBar4);
+            this.Controls.Add(this.hScrollBar3);
+            this.Controls.Add(this.hScrollBar2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.HScrollBar hScrollBar3;
+        private System.Windows.Forms.HScrollBar hScrollBar4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ week 4/CayleyTree/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CayleyTree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = this.CreateGraphics();
+            graphics.Clip = new Region(new Rectangle(10, 10, 1000, 2000));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if (graphics == null) graphics = this.CreateGraphics();
+            drawCayleyTree(vScrollBar1.Value, 300, 500, the_leng, -Math.PI / 2);
+        }
+
+        private Graphics graphics ;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+        double k = 0.8;//有道是"位置系数"
+        double the_leng=100;
+        Color color = Color.Black;//default_color:black
+
+        void drawCayleyTree(int n,
+                double x0, double y0, double leng, double th)
+        {
+            if (n == 0) return;
+
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+
+            drawLine(x0, y0, x1, y1);
+
+            double x2 = x0 + k*leng * Math.Cos(th);
+            double y2 = y0 + k*leng * Math.Sin(th);   //按照题目要求新增了x2y2点
+
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x2, y2, per2 * leng, th - th2);
+        }
+        Random colorrnd = new Random();
+        void drawLine(double x0, double y0, double x1, double y1)
+        {
+            //the following is designed to show random color for the tree
+            /*
+            int colornum = (int)colorrnd.Next(5);
+            Pen thepen=Pens.Black;
+            switch (colornum)                     //颜色随机五彩中的一个，营造缤纷效果
+            {
+                case 0:  thepen = Pens.Yellow;break;
+                case 1:  thepen = Pens.Blue; break;
+                case 2:  thepen = Pens.Red; break;
+                case 3:  thepen = Pens.Green; break;
+                case 4:  thepen = Pens.Pink; break;
+                default:
+                    break;
+            }
+            graphics.DrawLine(
+                thepen,
+                (int)x0, (int)y0, (int)x1, (int)y1);
+             */
+            Pen pen = new Pen(color);
+            graphics.DrawLine(pen, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+
+        private void hScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            th1 = (30+60*(double)hScrollBar1.Value/(double)91) * Math.PI / 180 ;            //之所以用91是因为滚动条的最大值只能到maximum+1-largechange
+            label5.Text = ((30 + 60 * (double)hScrollBar1.Value / (double)91)/180).ToString("f2")+"pi";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (graphics!=null)
+            {
+                graphics.Clear(this.BackColor);
+            }
+        }
+
+        private void hScrollBar2_ValueChanged(object sender, EventArgs e)
+        {
+            th2 = (20 + 70 * (double)hScrollBar2.Value / (double)91) * Math.PI / 180;
+            label6.Text = ((20 + 70 * (double)hScrollBar2.Value / (double)91) / 180).ToString("f2") + "pi";
+        }
+
+        private void hScrollBar3_ValueChanged(object sender, EventArgs e)
+        {
+            per1 =(double)hScrollBar3.Value / 91;
+            label7.Text = per1.ToString("f2");
+        }
+
+        private void hScrollBar4_ValueChanged(object sender, EventArgs e)
+        {
+            per2 = (double)hScrollBar4.Value / 91;
+            label8.Text = per2.ToString("f2");
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            k = (double)trackBar1.Value / 10;
+            label9.Text = "位置系数："+k.ToString("f2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+            {
+                color = MyDialog.Color;
+            }
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            label10.Text = "递归深度："+vScrollBar1.Value;
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            the_leng = trackBar2.Value;
+            label11.Text = "主干长度：" + trackBar2.Value;
+        }
+    }
+}
